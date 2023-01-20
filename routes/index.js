db7118774889c6680b6512ee1f79c5eb26379652
eugene-h-kim <h_kim123224@fanshaweonline.ca>
2023-01-19 20:49:43 -0500

    adding makeup class files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+let team = {
+
+  justin: {
+    name: 'Justin',
+    roles: 'coordinator'
+  },
+
+  joe: {
+    name: 'Joe',
+    roles: '3d/video productor/animator',
+    avatar: 'avatar_01.jpg'
+  },
+
+  john: {
+    name: 'John',
+    roles: '3d/video productor/animator'
+  },
+
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express',
+    content: 'express is so easy' });
 });
+
+
+router.get('/team/:member', function(req,res) {
+  console.log(req.params.member);
+
+res.render('team', { person: team[req.params.member]});
+})
 
 module.exports = router;
